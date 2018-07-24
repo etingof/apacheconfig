5fa5d4516ebb43a842edfed39ce14fd1326b2538
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2018-07-24 10:10:11 +0200

    Add TestInfraHostReader

--- apacheconfig/reader.py
+++ apacheconfig/reader.py
@@ -1,4 +1,5 @@
 import os
+import io
 
 
 class LocalHostReader(object):
@@ -23,3 +24,34 @@ class LocalHostReader(object):
 
     def open(self, filename, mode='r', bufsize=-1):
         return open(filename, mode, bufsize)
+
+
+class TestInfraHostReader(object):
+    """TestInfraHostReader for ApacheConfigLoader"""
+
+    def __init__(self, host):
+        self._host = host
+        self._env = host.env()
+
+    @property
+    def env(self):
+        return self._env
+
+    def exists(self, filepath):
+        return self._host.run_test("test -f %s", filepath).rc == 0
+
+    def isdir(self, filepath):
+        return self._host.run_test("test -d %s" % filepath).rc == 0
+
+    def listdir(self, filepath):
+        out = self._host.run_test("ls -A %s" % filepath)
+        if out.rc != 0:
+            raise OSError
+        else:
+            return out.stdout.split()
+
+    def open(self, filepath):
+        out = self.run_test("cat -- %s", filepath)
+        if out.rc != 0:
+            raise IOError
+        return io.StringIO(out.stdout)
