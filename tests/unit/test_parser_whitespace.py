6d03a2ac85a0385da0f04ff9994ee8ee93a30bfd
Ilya Etingof <etingof@gmail.com>
2019-10-04 09:49:50 +0200

    PEP8 tests and enable flake8 run on tests in CI

--- tests/unit/test_parser_whitespace.py
+++ tests/unit/test_parser_whitespace.py
@@ -4,9 +4,8 @@
 # Copyright (c) 2018-2019, Ilya Etingof <etingof@gmail.com>
 # License: https://github.com/etingof/apacheconfig/LICENSE.rst
 #
-import sys
-
-from apacheconfig import *
+from apacheconfig import make_lexer
+from apacheconfig import make_parser
 
 try:
     import unittest2 as unittest
@@ -18,22 +17,27 @@ except ImportError:
 def _create_parser(options={}, start='contents'):
     ApacheConfigLexer = make_lexer(**options)
     ApacheConfigParser = make_parser(**options)
+
     return ApacheConfigParser(ApacheConfigLexer(), start=start)
+
 
 class WhitespaceParserTestCase(unittest.TestCase):
     def _test_cases(self, cases, tag='contents', options={}):
         options['preservewhitespace'] = True
         parser = _create_parser(start=tag, options=options)
+
         for x, expected in cases:
             got = parser.parse(x)
-            self.assertEqual(got, expected,
-                "When parsing '%s' we got %s, but expected %s" %
-                    (repr(x), got, expected))
+
+            self.assertEqual(
+                got, expected,
+                "When parsing '%s' we got %s, but "
+                "expected %s" % (repr(x), got, expected))
 
     def testOptionAndValue(self):
         tests = [
-            ('a',  ['statement', 'a']),
-            ('a  b',  ['statement', 'a', '  ', 'b']),
+            ('a', ['statement', 'a']),
+            ('a  b', ['statement', 'a', '  ', 'b']),
             ('a = b', ['statement', 'a', ' = ', 'b']),
             ('a "b c"', ['statement', 'a', ' ', 'b c']),
         ]
@@ -41,17 +45,20 @@ class WhitespaceParserTestCase(unittest.TestCase):
 
     def testInclude(self):
         tests = [
-            ('include   file.conf',  ['include', 'include', '   ', 'file.conf']),
-            ('<<include   file.conf>>', ['include', '<<', 'include', '   ', 'file.conf', '>>']),
+            ('include   file.conf', [
+                'include', 'include', '   ', 'file.conf']),
+            ('<<include   file.conf>>', [
+                'include', '<<', 'include', '   ', 'file.conf', '>>']),
         ]
         self._test_cases(tests, tag='include')
 
     def testIncludeOptional(self):
         tests = [
-            ('includeoptional   file.conf',  ['includeoptional', 'includeoptional', '   ', 'file.conf']),
+            ('includeoptional   file.conf', [
+                'includeoptional', 'includeoptional', '   ', 'file.conf'
+            ])
         ]
         self._test_cases(tests, tag='includeoptional')
-
 
     def testHashComment(self):
         comment = '# hello there !!! yep'
@@ -68,8 +75,8 @@ class WhitespaceParserTestCase(unittest.TestCase):
             ('\n a b\n',
              ['contents', ['statement', '\n ', 'a', ' ', 'b'], '\n']),
             (' \n a b\nb c\n', ['contents',
-                         ['statement', ' \n ', 'a', ' ', 'b'],
-                         ['statement', '\n', 'b', ' ', 'c'], '\n']),
+                                ['statement', ' \n ', 'a', ' ', 'b'],
+                                ['statement', '\n', 'b', ' ', 'c'], '\n']),
         ]
         self._test_cases(tests, tag='contents')
 
@@ -79,23 +86,28 @@ class WhitespaceParserTestCase(unittest.TestCase):
             ('\n # a b', ['contents', ['comment', '\n ', '# a b']]),
             ('   # a b', ['contents', ['comment', '   ', '# a b']]),
             ('a b #comment\n  ', ['contents',
-                         ['statement', 'a', ' ', 'b'],
-                         ['comment', ' ', '#comment'], '\n  ']),
+                                  ['statement', 'a', ' ', 'b'],
+                                  ['comment', ' ', '#comment'], '\n  ']),
             ('a b \n #comment\n  ', ['contents',
-                         ['statement', 'a', ' ', 'b'],
-                         ['comment', ' \n ', '#comment'], '\n  ']),
-            ('a b #comment\n  #comment2\n c d\n', ['contents',
-                         ['statement', 'a', ' ', 'b'],
-                         ['comment', ' ', '#comment'],
-                         ['comment', '\n  ', '#comment2'],
-                         ['statement', '\n ', 'c', ' ', 'd'], '\n']),
+                                     ['statement', 'a', ' ', 'b'],
+                                     ['comment', ' \n ', '#comment'], '\n  ']),
+            ('a b #comment\n  #comment2\n c d\n', [
+                'contents',
+                ['statement', 'a', ' ', 'b'],
+                ['comment', ' ', '#comment'],
+                ['comment', '\n  ', '#comment2'],
+                ['statement', '\n ', 'c', ' ', 'd'], '\n'
+            ]),
         ]
         self._test_cases(tests, tag='contents')
 
     def testNamedBlocks(self):
         tests = [
-            ('<a name/>', ['block', ('a', ' ', 'name'), [], 'a name']),
-            ('<a name>\n</a>', ['block', ('a', ' ', 'name'), ['contents', '\n'], 'a']),
+            ('<a name/>', [
+                'block', ('a', ' ', 'name'), [], 'a name'
+            ]),
+            ('<a name>\n</a>', [
+                'block', ('a', ' ', 'name'), ['contents', '\n'], 'a']),
         ]
         self._test_cases(tests, tag='block')
 
@@ -103,18 +115,18 @@ class WhitespaceParserTestCase(unittest.TestCase):
         tests = [
             ('<a/>', ['block', ('a',), [], 'a']),
             ('<a>\n</a>', ['block', ('a',), ['contents', '\n'], 'a']),
-            ('<a> #comment\n</a>',
-             ['block', ('a',), ['contents',
-                        ['comment', ' ', '#comment'], '\n'], 'a']),
-            ('<a> #comment\n a b #comment2\n</a>',
-             ['block', ('a',), ['contents',
-                        ['comment', ' ', '#comment'],
-                        ['statement', '\n ', 'a', ' ', 'b'],
-                        ['comment', ' ', '#comment2'], '\n'], 'a']),
+            ('<a> #comment\n</a>', [
+                'block', ('a',), ['contents', [
+                    'comment', ' ', '#comment'], '\n'], 'a']),
+            ('<a> #comment\n a b #comment2\n</a>', [
+                'block', ('a',), ['contents', [
+                    'comment', ' ', '#comment'],
+                                  ['statement', '\n ', 'a', ' ', 'b'],
+                                  ['comment', ' ', '#comment2'], '\n'], 'a'])
         ]
+
         self._test_cases(tests, tag='block')
 
     def testEmptyConfig(self):
         tests = [(" \n", ['config', ['contents', ' \n']])]
         self._test_cases(tests, tag='config')
-
