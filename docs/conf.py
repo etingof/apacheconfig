04c402999067fdb51673259e9cdaca6a7dfc324f
sydneyli <sydney@eff.org>
2019-10-08 09:24:07 -0700

    Add napoleon extension and fix travis build

--- docs/conf.py
+++ docs/conf.py
@@ -33,13 +33,15 @@ release = u''
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+# Needs at least 1.3 for sphinx.ext.napoleon
+needs_sphinx = '1.3'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
