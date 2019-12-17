4337810f057474b1231eda59011a9a24868c6717
sydneyli <sydney@eff.org>
2019-12-17 11:41:05 -0800

    Actually forward exit code on test runs

--- tests/__main__.py
+++ tests/__main__.py
@@ -17,4 +17,8 @@ suite = unittest.TestLoader().loadTestsFromNames(
 
 
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    if result.wasSucccessful():
+        exit(0)
+    else:
+        exit(1)
